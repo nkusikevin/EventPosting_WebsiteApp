ed7a982918d18358ce37bb9cc0ff6ed58efebee7
“nkusikevin” <nkusikvnhart@gmail.com>
2021-10-12 23:00:31 +0200

    fetching single user events only

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,11 +1,29 @@
 import React from "react";
-
+import { API_URL } from "@/config/index";
+import {parseCookies} from "@/helper/index"
 import Layout from "@/components/Layout";
 
-export default function dashboard() {
+export default function dashboard({events}) {
+	console.log(events);
 	return (
 		<Layout title='Dashboard'>
 			<h1>Welcome</h1>
 		</Layout>
 	);
+}
+
+
+export  async function getServerSideProps({req}){
+const {token} = parseCookies(req)
+const res = await fetch(`${API_URL}/events/me`,{
+	method:'GET',
+	Authorization:`Bearer ${token}`
+})
+
+const events = await res.json()
+return {
+	props:{
+		events
+	}
+}
 }
